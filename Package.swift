98cae0fe6026cb23ad80393c1d1adc272ef0df9f
timi2506 <172171055+timi2506@users.noreply.github.com>
2026-03-18 08:28:04 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-    name: "SwiftPing"
+    name: "SwiftPing",
+    products: [
+        .library(
+            name: "SwiftPing",
+            targets: ["SwiftPing"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "SwiftPing",
+            path: "SwiftPing"
+        )
+    ]
 )
